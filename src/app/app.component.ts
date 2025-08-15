0d13afa4ca448c58b35c98b984301aa8052446b6
mohd-3384 <mohd3384@hotmail.com>
2025-08-15 23:01:41 +0200

    update: hero and about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from "./features/about/about.component";
 import { HeaderComponent } from './features/header/header.component';
 import { FooterComponent } from './features/footer/footer.component';
+import { HeroComponent } from "./features/hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
